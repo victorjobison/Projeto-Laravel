b68fb21d9473cc85d359a03cde75b96f8a194e23
victorjobison <victorjobison2016@gmail.com>
2024-05-16 16:29:05 -0300

    Configurando tela inicial

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        hmr: {
-            host: 'vps1.saudelog.zamed.local.br',
-        }
-    },
     plugins: [
         laravel({
             input: [
